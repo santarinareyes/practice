6b1b8eb5e753b579b11e84d8858a6f4a36522365
Richard <richard.santarinareyes@medieinstitutet.se>
2021-04-02 19:34:50 +0200

    playing with jquery styling

--- jQuery/script.js
+++ jQuery/script.js
@@ -102,3 +102,16 @@ $("input[name='interest']").change(function () {
     );
   });
 });
+
+// Add/Remove/Toggle class buttons
+$("#add_class").click(function () {
+  $("#example-paragraph").addClass("styling");
+});
+
+$("#remove_class").click(function () {
+  $("#example-paragraph").removeClass("styling");
+});
+
+$("#toggle_class").click(function () {
+  $("#example-paragraph").toggleClass("styling");
+});
