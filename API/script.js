b5ee7a8fc8b6fd37655fb3b46a84056d68d74bcb
Richard <richard.santarinareyes@medieinstitutet.se>
2021-04-03 11:55:27 +0200

    testing

--- API/script.js
+++ API/script.js
@@ -1,12 +1,16 @@
 "use_strict";
 
-let xhr = new XMLHttpRequest();
-let url = "https://api.chucknorris.io/jokes/random";
+let response = document.querySelector("#api-response"),
+  xhr = new XMLHttpRequest(),
+  url = "https://api.chucknorris.io/jokes/random";
 
 xhr.onreadystatechange = function () {
   console.log(xhr.readyState);
-  if (xhr.readyState === 4) {
-    console.log(xhr.response);
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    // console.log(xhr.response);
+    // console.log(xhr.responseText);
+    let obj = JSON.parse(xhr.responseText);
+    response.innerHTML = obj.url;
   }
 };
 
