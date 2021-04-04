1afc45e02c2279041fd58b2396c2ecf6c199cec3
Richard <richard.santarinareyes@medieinstitutet.se>
2021-04-04 16:00:03 +0200

    testing

--- API/script.js
+++ API/script.js
@@ -1,19 +1,84 @@
 "use_strict";
 
-let response = document.querySelector("#api-response"),
-  xhr = new XMLHttpRequest(),
-  url = "https://api.chucknorris.io/jokes/random";
+// document.querySelector("button").onclick = function () {
+//   let response = document.querySelector("#api-response"),
+//     xhr = new XMLHttpRequest(),
+//     url = "https://api.chucknorris.io/jokes/random";
 
-xhr.onreadystatechange = function () {
-  console.log(xhr.readyState);
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    // console.log(xhr.response);
-    // console.log(xhr.responseText);
-    let obj = JSON.parse(xhr.responseText);
-    response.innerHTML = obj.url;
-  }
+//   xhr.onreadystatechange = function () {
+//     console.log(xhr.readyState);
+//     if (xhr.readyState === 4 && xhr.status === 200) {
+//       // console.log(xhr.response);
+//       // console.log(xhr.responseText);
+//       let obj = JSON.parse(xhr.responseText);
+//       console.log(obj);
+//       response.innerHTML = obj.value;
+//     }
+//   };
+
+//   xhr.open("GET", url);
+//   xhr.send();
+//   console.log(xhr);
+// };
+
+// document.querySelector("button").onclick = function () {
+//   let response = document.querySelector("#api-response"),
+//     xhr = new XMLHttpRequest(),
+//     url = "https://api.chucknorris.io/jokes/random";
+
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState === 4) {
+//       if (xhr.status === 200) {
+//         let obj = JSON.parse(xhr.responseText);
+//         response.innerHTML = obj.value;
+//       } else {
+//         response.innerHTML = "ERROR";
+//       }
+//     }
+//   };
+
+//   xhr.open("GET", url);
+//   xhr.send();
+
+//   xhr.onprogress = function (e) {
+//     const test = e.timeStamp;
+//     console.log(realTime);
+//     console.log(test);
+//   };
+
+//   xhr.onerror = function (e) {
+//     console.log(e);
+//   };
+
+// xhr.addEventListener("progress", function (e) {
+//   console.log(e);
+// });
+// };
+
+const btn = document.querySelector("button"),
+  input = document.querySelector("input"),
+  response = document.querySelector("#api-response"),
+  url = "https://randomuser.me/api/";
+
+btn.onclick = function () {
+  const xhr = new XMLHttpRequest(),
+    amountToGet = "?results=" + input.value;
+  xhr.onload = function () {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        let data = JSON.parse(xhr.responseText).results;
+        getValues(data);
+      }
+    }
+  };
+  xhr.open("GET", url + amountToGet);
+  xhr.send();
 };
 
-xhr.open("GET", url);
-xhr.send();
-console.log(xhr);
+function getValues(data) {
+  response.innerHTML = "<ul>";
+  for (i = 0; i < data.length; i++) {
+    response.innerHTML += "<li>" + data[i].email + "</li>";
+  }
+  response.innerHTML += "</ul>";
+}
