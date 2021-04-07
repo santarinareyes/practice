28491bc140609977b4a707bda577aa016451ce64
Richard <richard.santarinareyes@medieinstitutet.se>
2021-04-07 19:05:00 +0200

    fetching test

--- Strapi/frontend/src/pages/SinglePost.js
+++ Strapi/frontend/src/pages/SinglePost.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import Post from "../components/Post";
 
@@ -5,24 +6,33 @@ const SinglePost = ({ match }) => {
   const url = "http://localhost:1337";
   const { id } = match.params;
   const [post, setPost] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPost = async () => {
       const response = await fetch(`${url}/posts/${id}`);
       const data = await response.json();
       setPost(data);
-      console.log(data);
     };
+    setLoading(false);
     fetchPost();
-  });
+  }, []);
 
   return (
     <div>
-      <Post
-        description={post.description}
-        url={post.image && post.image.url}
-        likes={post.likes}
-      />
+      {loading && <p>Loading...</p>}
+      {!loading && (
+        <>
+          {post.id && (
+            <Post
+              description={post.description}
+              url={post.image && post.image.url}
+              likes={post.likes}
+            />
+          )}
+          {!post.id && <p styles="color: red">404 - not found</p>}
+        </>
+      )}
     </div>
   );
 };
